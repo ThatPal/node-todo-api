4054c03c561f248e35faa24128ccb5d663e7e9e9
Mordechai Pal <mordechaipal@gmail.com>
2018-09-20 16:07:44 -0400

    Add tests for GET /users/me and POST /users route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,25 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
 const {ObjectID} = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {User} = require('./../models/user');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
-const todos = [{
-    _id: new ObjectID(),
-    text: 'First test todo'
-}, {
-    _id: new ObjectID(),
-    text: 'Second test todo',
-    completed: true,
-    completedAt: 321456987
-}];
-
-beforeEach(done => {
-    Todo.deleteMany({}).then(() => {
-        return Todo.insertMany(todos);
-    }).then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
     it('should create a new todo', done => {
@@ -211,6 +201,85 @@ describe('PATCH /todos/:id', () => {
         request(app)
             .patch('/todos/123abc')
             .expect(404)
+            .end(done);
+    });
+});
+
+describe('GET /users/me', () => {
+    it('should return user if authenticated', done => {
+        request(app)
+            .get(`/users/me`)
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .expect(res => {
+                expect(res.body._id).toBe(users[0]._id.toHexString());
+                expect(res.body.email).toBe(users[0].email);
+            })
+            .end(done);
+    });
+
+    it('should return 401 if not authenticated', done => {
+        request(app)
+            .get(`/users/me`)
+            .expect(401)
+            .expect(res => {
+                expect(res.body).toEqual({});
+            })
+            .end(done);
+    });
+});
+
+describe('POST /users', () => {
+    it('should create a user', done => {
+        let email = 'test@suite.run';
+        let password = 'password123';
+        
+        request(app)
+            .post(`/users`)
+            .send({email, password})
+            .expect(200)
+            .expect(res => {
+                expect(res.header['x-auth']).toBeTruthy();
+                expect(res.body._id).toBeTruthy();
+                expect(res.body.email).toBe(email);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findOne({email}).then(user => {
+                    expect(user).toBeTruthy();
+                    bcrypt.compare(password, user.password, (err, res) => {
+                        expect(res).toBe(true);
+                    });
+                    expect(user._id.toHexString()).toBe(res.body._id);
+                    expect(user.tokens[0].token).toBe(res.header['x-auth']);
+                    
+                    done();
+                }).catch(e => done(e));
+            });
+    });
+
+    it('should return 400 for invalid request', done => {
+        let email = 'Not an email';
+        let password = '1';
+        
+        request(app)
+            .post(`/users`)
+            .send({email, password})
+            .expect(400)
+            .end(done);
+    });
+
+    it('should return 400 for existing email', done => {
+        let email = users[0].email;
+        let password = 'password123';
+        
+        request(app)
+            .post(`/users`)
+            .send({email, password})
+            .expect(400)
             .end(done);
     });
 });
